0e20b83a93fa6ee3d5e5947e52c8ede4ec3dbcf8
Pratyush kumar <pratyush3030@gmail.com>
2017-06-30 03:07:05 +0000

    Profile database updated

---
+++ db/migrate/20170630030338_create_profilers.rb
@@ -0,0 +1,15 @@
+class CreateProfilers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profilers do |t|
+      t.integer :user_id
+      t.string :user_id
+      t.string :first_name
+      t.string :last_name
+      t.string :job_title
+      t.string :phone_number
+      t.string :contact_email
+      t.text :description
+      t.timestamps
+    end
+  end
+end
